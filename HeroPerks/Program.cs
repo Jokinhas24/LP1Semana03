1a70e4674638b82b2b893c80090baf928ff7f04d
João Nogueira <joaonogueira.art@gmail.com>
2024-03-07 22:57:06 +0000

    Added last print

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -2,59 +2,67 @@
 
 namespace HeroPerks
 {
-     [Flags]
-            public enum Perks
-            {
-            None = 0,
-            WaterBreathing = 1,
-            Stealth = 2,
-            AutoHeal = 4,
-            DoubleJump = 8
-            }
+    [Flags]
+    public enum Perks
+    {
+        None = 0,
+        WaterBreathing = 1,
+        Stealth = 2,
+        AutoHeal = 4,
+        DoubleJump = 8
+    }
     class Program
     {
         static void Main(string[] args)
         {
             if (args.Length == 0)
-        {
-            Console.WriteLine("Player has no perks");s
-            return;
-        }
+            {
+                Console.WriteLine("Player has no perks");
+                return;
+            }
             Perks playerPerks = Perks.None;
 
             foreach (char perkChar in args[0])
-        {
-            switch (perkChar)
             {
-                case 'w':
-                    playerPerks ^= Perks.WaterBreathing;
-                    break;
-                case 'a':
-                    playerPerks ^= Perks.Stealth;
-                    break;
-                case 's':
-                    playerPerks ^= Perks.AutoHeal;
-                    break;
-                case 'd':
-                    playerPerks ^= Perks.DoubleJump;
-                    break;
-                default:
-                    Console.WriteLine("Unknown perk!");
-                    return;
+                switch (perkChar)
+                {
+                    case 'w':
+                        playerPerks ^= Perks.WaterBreathing;
+                        break;
+                    case 'a':
+                        playerPerks ^= Perks.Stealth;
+                        break;
+                    case 's':
+                        playerPerks ^= Perks.AutoHeal;
+                        break;
+                    case 'd':
+                        playerPerks ^= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown perk!");
+                        return;
+                }
             }
-        }   
-             if (playerPerks == Perks.None)
-        {
-            Console.WriteLine("No perks at all!");
-        }
-            else
-        {
-            Console.WriteLine($"Player has this perks: {playerPerks}");
-
-            if ((playerPerks & Perks.Stealth) != 0 && (playerPerks & Perks.DoubleJump) != 0)
+            if (playerPerks == Perks.None)
             {
-                Console.WriteLine("Silent jumper!");
+                Console.WriteLine("No perks at all!");
+            }
+            else
+            {
+                Console.WriteLine($"Player has this perks: {playerPerks}");
+
+                if ((playerPerks & Perks.Stealth) != 0 && (playerPerks & Perks.DoubleJump) != 0)
+                {
+                    Console.WriteLine("Silent jumper!");
+                }
+            
+                if ((playerPerks & Perks.AutoHeal) == 0)
+                {
+                    Console.WriteLine("Not gonna make it!");
+                }
             }
         }
     }
 }
+
+
