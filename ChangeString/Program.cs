b4366c23c44082f45682a0cd265cd255bd231735
João Nogueira <joaonogueira.art@gmail.com>
2024-03-07 20:58:07 +0000

    Added asking string and char then write the string

---
+++ ChangeString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChanceString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please write a sentence: ");
+            string str = Console.ReadLine();
+            Console.WriteLine("Now write a character: ");
+            string chr = Console.ReadLine();
+            
+            foreach chr in str:
+            {
+                str -= chr;
+            }
+
+            Console.WriteLine("Modified Sentence:");
+            Console.Write(str);
+        }
+    }
+}
