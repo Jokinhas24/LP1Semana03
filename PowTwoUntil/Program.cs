5f9edeb570e20a28f032c53997e3ebe42f5aae10
João Nogueira <joaonogueira.art@gmail.com>
2024-03-08 13:24:31 +0000

    Added Pow until 5 code

---
+++ PowTwoUntil/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PowTwoUntil
+{
+    class Program
+    {
+        private static void PowersOf2Until5()
+        {
+            for (int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    
+        static void Main(String[] args)
+        {
+            PowersOf2Until5();
+        }
+    }
+}
+
