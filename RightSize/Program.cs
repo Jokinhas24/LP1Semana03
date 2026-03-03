c018f163664e1d160c527a03914cfe7a3fdf89aa
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-03 12:18:58 +0000

    1st exercise

---
+++ RightSize/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            foreach (string s in args)
+            {
+                if (s.Length < 3)
+                {
+                    continue;
+                }
+                else if (s.Length > 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            ;
+        }
+    }
+}
