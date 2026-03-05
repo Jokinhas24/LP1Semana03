a0cb977bba2aa90745a5d6642eb75095986f690b
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-05 15:05:02 +0000

    Remove char to x's

---
+++ ChangeString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ChangeString
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("String: ");
+            string s = Console.ReadLine();
+            Console.Write("Character: ");
+            char c = char.Parse(Console.ReadLine());
+
+            char x = char.Parse("x");
+            s = s.Replace(c, x);
+
+            Console.Write("Impressed: " + s);
+            
+            
+        }
+    }
+}
