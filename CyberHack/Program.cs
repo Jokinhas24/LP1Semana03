7b5143075c7829785238cedd7438016adcd03bb8
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-05 16:52:46 +0000

    Corporate Server checking

--- CyberHack/Program.cs
+++ CyberHack/Program.cs
@@ -7,13 +7,35 @@ namespace CyberHack
         private static void Main(string[] args)
         {
             Enum.TryParse(Console.ReadLine(), out SystemType system);
-            Enum.TryParse(Console.ReadLine(), out HackTool hack);
+
+            string[] hacks = new string(Console.ReadLine());
+
+            //hacks.Split(" ");
+            
+            foreach (string h in hacks)
+            {
+                Console.WriteLine(h);
+                Enum.TryParse(h, out HackTool hack);
+            }
+
 
             if (system == SystemType.CorporateServer)
             {
-                
+                foreach (HackTool hk in hacks)
+                    if ((hk == HackTool.Phishing) || (hk == HackTool.Backdoor))
+                        {
+                            if ((hk == HackTool.BruteForce) || (hk == HackTool.ZeroDay))
+                            {
+                             Console.WriteLine("System Hacked");
+                            }
+                        }
+                    else
+                    {
+                        Console.WriteLine("System Secured");
+                    }
+                    
             }
-            Console.WriteLine("System Hacked");
+            
         }
     }
 }
