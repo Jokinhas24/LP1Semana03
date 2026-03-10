1491546442afd61e940fbcd5d4e4a5dd6b5d406a
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-10 11:21:37 +0000

    Add new projects

---
+++ Pow2Until/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Pow2Until
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            PowerOf2Until5();
+            PowerOf2Until5();
+        }
+        private static void PowerOf2Until5()
+        {
+            for(int i = 1; i <= (1 << 5); i = i << 1)
+            {
+                Console.WriteLine(1);
+            }
+        }
+    }
+}
