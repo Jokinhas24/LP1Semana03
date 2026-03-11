5c3803accd929bf2a612f61b2b290fe3e4f3616e
[João Nogueira] <146846348+Jokinhas24@users.noreply.github.com>
2026-03-11 23:33:11 +0000

    perks shown and all works as intended

--- HeroPerk/Program.cs
+++ HeroPerk/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.Json.Serialization;
 
 namespace HeroPerk
 {
@@ -6,7 +7,52 @@ namespace HeroPerk
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP!");
+            if (args.Length == 0)
+            {
+                Console.WriteLine("!No perks at all!");
+            }
+            Perks playerPerks = 0;
+
+            foreach (char perkChar in args[0])
+            {
+                switch (perkChar)
+                {
+                    case 'w':
+                        playerPerks ^= Perks.WarpShift;
+                        break;
+                    case 'a':
+                        playerPerks ^= Perks.AutoHeal;
+                        break;
+                    case 's':
+                        playerPerks ^= Perks.Stealth;
+                        break;
+                    case 'd':
+                        playerPerks ^= Perks.DoubleJump;
+                        break;
+                    default:
+                        Console.WriteLine("Unknown perk!");
+                        return;
+                }
+            }
+            if (playerPerks == 0)
+            {
+                Console.WriteLine("No perks at all!");
+            }
+            else
+            {
+            Console.WriteLine(playerPerks);
+            }
+
+            if ((playerPerks & Perks.Stealth) != 0 &&
+                (playerPerks & Perks.DoubleJump) != 0)
+            {
+                Console.WriteLine("!Silent jumper!");
+            }
+
+            if (playerPerks != Perks.AutoHeal)
+            {
+                 Console.WriteLine("!Not gonna make it");
+            }
         }
     }
 }
